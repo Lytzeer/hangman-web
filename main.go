86391a09ac0b4e2e50a75eb11511cbad0ce4176e
m4ul <hansonlucas06@gmail.com>
2022-11-14 12:07:21 +0100

    updated post

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func main() {
+
 	http.HandleFunc("/hangman", Handler)
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
@@ -15,8 +15,14 @@ func main() {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.Method)
-	v := r.FormValue("input")
-	fmt.Println(v)
-
+	switch r.Method {
+	case "GET":
+		fmt.Println("GET")
+	case "POST": // Gestion d'erreur
+		if err := r.ParseForm(); err != nil {
+			return
+		}
+	}
+	variable := r.Form.Get("input")
+	fmt.Println(variable)
 }
