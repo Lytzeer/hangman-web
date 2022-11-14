b0edfe15cc05363b74b97ac05e598febd40dea75
lmatheo <matheo.lang@ynov.com>
2022-11-14 11:30:41 +0100

    post request

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
-
+	http.HandleFunc("/hangman", Handler)
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Method)
 }
