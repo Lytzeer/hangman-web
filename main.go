3def2a73be92fb0a33a4cfc29b860000dc71418c
lmatheo <matheo.lang@ynov.com>
2022-11-14 12:25:56 +0100

    go request

--- main.go
+++ main.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type Test struct {
+	letter string
+}
+
 func main() {
+	http.HandleFunc("/", Handler)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/hangman", Handler)
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
-
 	http.ListenAndServe(":8080", nil)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./static/index.html"))
+	data := Test{
+		letter: "Ouba ouba",
+	}
+	tmpl.Execute(w, data)
 	switch r.Method {
 	case "GET":
 		fmt.Println("GET")
@@ -23,6 +33,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+	// Récupérez votre valeur
 	variable := r.Form.Get("input")
 	fmt.Println(variable)
 }
